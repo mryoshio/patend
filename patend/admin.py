e6cfbc03a6f6212bac5e0799d8e9ad7da7c02f3b
mryoshio <yoshiokaas@gmail.com>
2016-05-20 12:05:38 +0900

    enable model management by Web UI

--- patend/admin.py
+++ patend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Company, Office, Record, User
+
+admin.site.register(Company)
+admin.site.register(Office)
+admin.site.register(Record)
+admin.site.register(User)
--- patend/models.py
+++ patend/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=30)
     pricing = models.BooleanField(default=False)
+    def __str__(self):
+        return self.name
 
 class Office(models.Model):
     name = models.CharField(max_length=30)
