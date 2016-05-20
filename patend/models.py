0ce881215f0c9272a00b5c3e1dd016605783e38b
mryoshio <yoshiokaas@gmail.com>
2016-05-20 11:38:48 +0900

    create 1st migration

--- patend/models.py
+++ patend/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+    pricing = models.BooleanField(default=False)
+
+class Office(models.Model):
+    name = models.CharField(max_length=30)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    token = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+class Record(models.Model):
+    office = models.ForeignKey(Office)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- tk_patend/settings.py
+++ tk_patend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'patend.apps.PatendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'tk_patend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'patend',
+        'USER': 'root',
+        'PASSWORD': 'passwd',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'TEST': {
+            'NAME': 'patend_test',
+        },
     }
 }
 
